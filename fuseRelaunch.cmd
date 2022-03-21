5acc65292da354773df6b154f64e81bb71806503
aditiangadi <59868871+aditiangadi@users.noreply.github.com>
2022-03-21 12:19:00 +0530

    Add files via upload

---
+++ fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "C:/project/gcd/gcd_datapath_isim_beh.exe" -prj "C:/project/gcd/gcd_datapath_beh.prj" "work.gcd_datapath" "work.glbl" 
